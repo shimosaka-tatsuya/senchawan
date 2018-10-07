1e3fd1a5b2616a34e57bb2e52914453ea6390ca7
下坂　辰弥 <>
2018-10-08 02:14:24 +0900

    no message

--- _wp/app/public/wp-content/themes/senchawan/header.php
+++ _wp/app/public/wp-content/themes/senchawan/header.php
@@ -1,24 +1,13 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" type="text/css" href="/css/columnDetail.css">
+<link rel="stylesheet" type="text/css" href="/css/base.min.css">
+
+<?php // 記事ページのスタイル ?>
+<?php if( is_single() ) : ?><link rel="stylesheet" type="text/css" href="/css/column-detail.min.css"><?php endif; ?>
 
 <?php
 	wp_deregister_script( 'jquery' ); 
@@ -26,3 +15,46 @@
 ?>
 </head>
 <body>
+
+<header class="box-header">
+	<a class="box-siteLogo" href="/">ロゴ</a><!-- /.box-siteLogo -->
+	
+	<input id="hamburgerMenuInput" type="checkbox">
+	
+	<label class="btn-hamburgerMenu" for="hamburgerMenuInput"><span>メニュー</span></label>
+	
+	<nav class="box-hamburgerMenu">
+		
+		<ul class="list-hamburgerMenu">
+			<li class="btn-hamburgerMenu">
+				<a class="btn-hamburgerMenuInr" href="/">トップ</a><!-- /.btn-hamburgerMenuInr -->
+			</li><!-- /.btn-hamburgerMenu -->
+			
+			<li class="btn-hamburgerMenu">
+				<a class="btn-hamburgerMenuInr" href="/about/">アバウト</a><!-- /.btn-hamburgerMenuInr -->
+			</li><!-- /.btn-hamburgerMenu -->
+			
+			<li class="box-hamburgerMenuColumn">
+				<p class="ttl-hamburgerMenuColumn">コラム</p><!-- /.ttl-hamburgerMenuColumn -->
+				<ul class="list-hamburgerMenuColumn">
+					<li class="btn-hamburgerMenuColumn">
+						<a class="btn-hamburgerMenuColumnInr" href="/column/">全コラム一覧</a><!-- /.btn-hamburgerMenuColumnInr -->
+					</li><!-- /.btn-hamburgerMenuColumn -->
+					
+					<li class="btn-hamburgerMenuColumn">
+						<a class="btn-hamburgerMenuColumnInr" href="/category/culture/">カルチャー</a><!-- /.btn-hamburgerMenuColumnInr -->
+					</li><!-- /.btn-hamburgerMenuColumn -->
+					
+					<li class="btn-hamburgerMenuColumn">
+						<a class="btn-hamburgerMenuColumnInr" href="/category/news/">ニュース</a><!-- /.btn-hamburgerMenuColumnInr -->
+					</li><!-- /.btn-hamburgerMenuColumn -->
+					
+					<li class="btn-hamburgerMenuColumn">
+						<a class="btn-hamburgerMenuColumnInr" href="/category/product/">プロダクト</a><!-- /.btn-hamburgerMenuColumnInr -->
+					</li><!-- /.btn-hamburgerMenuColumn -->
+				</ul><!-- /.list-hamburgerMenuColumn -->
+			</li><!-- /.box-hamburgerMenuColumn -->
+		</ul><!-- /.list-hamburgerMenu -->
+		
+	</nav><!-- /.box-hamburgerMenu -->
+</header><!-- /.box-header -->
--- _wp/app/public/wp-content/themes/senchawan/index.php
+++ _wp/app/public/wp-content/themes/senchawan/index.php
@@ -1,10 +1,60 @@
 <?php get_header(); ?>
 
-<!-- 最新記事を取得 -->
+<!-- 最新記事3件を取得 -->
 <?php
   $args = array(
     'post_type' => 'post',
-    'posts_per_page' => 6
+    'posts_per_page' => 3
+  );
+  $st_query = new WP_Query( $args );
+?>
+<div class="box-mainVisual">
+<?php if ( $st_query->have_posts() ): ?>
+	<?php while ( $st_query->have_posts() ) : $st_query->the_post(); ?>
+	<div class="box-mainVisualInr">
+		<a href="<?php the_permalink(); ?>">
+			<img class="img-mainVisual" src="<?php $eye_img = wp_get_attachment_image_src( get_post_thumbnail_id() , 'large' ); print_r($eye_img[0]); ?>" alt="『<?php the_title(); ?>』のサムネイル / thumbnail of '<?php the_field('ttlColumnEnglish'); ?>'" width="100%" height="">
+			
+			<p class="txt-mainVisualCategory"><?php echo get_cat_name(get_the_category()[0]->term_id); ?></p>
+			
+			<p class="ttl-mainVisual-english"><?php the_field('ttlColumnEnglish'); ?></p>
+			
+			<p class="ttl-mainVisual-japanese"><?php the_title(); ?></p>
+		</a>
+		<p class="txt-mainVisualDate"><?php the_time('Y.m.d'); ?></p>
+	</div><!-- /.box-mainVisualInr -->
+	<?php endwhile; ?>
+	<?php else: ?>
+	<div class="box-mainVisualInr">
+		<a href="<?php the_permalink(); ?>">
+			<img class="img-mainVisual" src="/img/dummy.jpg" alt="ダミーのサムネイル" width="100%" height="">
+			
+			<p class="ttl-mainVisual-english">There is no new article</p>
+			
+			<p class="ttl-mainVisual-japanese">新しい記事はありません</p>
+		</a>
+	</div><!-- /.box-mainVisualInr -->
+<?php endif; ?>
+</div><!-- /.box-mainVisual -->
+
+<div class="box-about">
+	<dl class="data-about">
+		<dt class="ttl-About">About</dt>
+		<dd class="txt-about txt-about-english">I will input the ABOUT from here.I will input the ABOUT from here.I will input the ABOUT from here.</dd>
+		<dd class="txt-about txt-about-japanese">テキスト入りますテキスト入りますテキスト入りますテキスト入ります</dd>
+	</dl><!-- /.data-about -->
+	
+	<a class="btn-detail" href="/about/">詳細を見る</a>
+</div><!-- /.box-About -->
+
+<p class="ttl-sectionHead">column</p>
+
+<!-- その他の最新記事を取得 -->
+<?php
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 6,
+    'offset' => 3 //1〜3件目の記事は表示しない。
   );
   $st_query = new WP_Query( $args );
 ?>
@@ -26,4 +76,33 @@
 <?php endif; ?>
 </ul><!-- /.list-newColumn -->
 
+<a class="btn-detail" href="/column/">コラムをもっと見る</a>
+
+<p class="ttl-sectionHead">instagram</p>
+
+<div class="box-Instagram">
+	インスタグラム（後ほど実装）
+</div><!-- /.box-Instagram -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/js/jquery.bxslider.min.js"></script>
+<script>
+  $(document).ready(function(){
+    $('.box-mainVisual').bxSlider({
+	    mode: "horizontal",
+	    auto: true,
+	    speed: "500",
+	    pause: "3000",
+	    infiniteLoop: true,
+	    captions: false,
+	    responsive: true,
+	    touchEnabled: true,
+	    pager: true,
+	    controls: true,
+	    nextText: "次へ",
+	    prevText: "前へ",
+	    easing: "ease"
+    });
+  });
+</script>
 <?php get_footer(); ?>
