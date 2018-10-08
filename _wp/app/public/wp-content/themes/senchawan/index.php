c6f1a576cf4bf32110cc7f32964911e43581aca3
下坂　辰弥 <>
2018-10-08 22:04:52 +0900

    ◎トップの新着コラムを修正 ・モジュール化

--- _wp/app/public/wp-content/themes/senchawan/index.php
+++ _wp/app/public/wp-content/themes/senchawan/index.php
@@ -61,15 +61,10 @@
 <ul class="list-newColumn">
 <?php if ( $st_query->have_posts() ): ?>
 	<?php while ( $st_query->have_posts() ) : $st_query->the_post(); ?>
-	<li class="box-newColumn">
-		<a href="<?php the_permalink(); ?>">
-			<img class="img-mainVisual" src="<?php $eye_img = wp_get_attachment_image_src( get_post_thumbnail_id() , 'medium' ); print_r($eye_img[0]); ?>" alt="『<?php the_title(); ?>』のサムネイル / thumbnail of '<?php the_field('ttlColumnEnglish'); ?>'" width="300" height="">
-			<p class="txt-otherColumnCategory"><?php echo get_cat_name(get_the_category()[0]->term_id); ?></p>
-			<p class="ttl-newColumn-english"><?php the_field('ttlColumnEnglish'); ?></p>
-			<p class="ttl-newColumn-japanese"><?php the_title(); ?></p>
-		</a>
-		<p class="txt-newColumnDate"><?php the_time('Y.m.d'); ?></p>
-	</li><!-- /.box-newColumn -->
+
+	<?php // コラム ?>
+	<?php get_template_part( 'template-parts/list-column/box-column', get_post_format() ); ?>
+
 	<?php endwhile; ?>
 	<?php else: ?>
 	<li class="box-newColumn-none">新しい記事はありません</li><!-- /.box-newColumn-none -->
