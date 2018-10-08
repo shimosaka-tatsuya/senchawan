0d081b4049be9418b12362c976992362df7a91fe
下坂　辰弥 <>
2018-10-08 21:45:59 +0900

    no message

--- _wp/app/public/wp-content/themes/senchawan/header.php
+++ _wp/app/public/wp-content/themes/senchawan/header.php
@@ -26,30 +26,30 @@
 	<nav class="box-hamburgerMenu">
 		
 		<ul class="list-hamburgerMenu">
-			<li class="btn-hamburgerMenu">
+			<li class="btn-hamburgerMenu <?php if (is_home() && !is_paged()) { ?> btn-hamburgerMenu-current<?php } ?>">
 				<a class="btn-hamburgerMenuInr" href="/">トップ</a><!-- /.btn-hamburgerMenuInr -->
 			</li><!-- /.btn-hamburgerMenu -->
 			
-			<li class="btn-hamburgerMenu">
+			<li class="btn-hamburgerMenu<?php if(is_page( '453' )): ?> btn-btn-hamburgerMenu-current<?php endif; ?>">
 				<a class="btn-hamburgerMenuInr" href="/about/">アバウト</a><!-- /.btn-hamburgerMenuInr -->
 			</li><!-- /.btn-hamburgerMenu -->
 			
 			<li class="box-hamburgerMenuColumn">
 				<p class="ttl-hamburgerMenuColumn">コラム</p><!-- /.ttl-hamburgerMenuColumn -->
 				<ul class="list-hamburgerMenuColumn">
-					<li class="btn-hamburgerMenuColumn">
+					<li class="btn-hamburgerMenuColumn<?php if(is_page( '2' )): ?> btn-hamburgerMenuColumn-current<?php endif; ?>">
 						<a class="btn-hamburgerMenuColumnInr" href="/column/">全コラム一覧</a><!-- /.btn-hamburgerMenuColumnInr -->
 					</li><!-- /.btn-hamburgerMenuColumn -->
 					
-					<li class="btn-hamburgerMenuColumn">
+					<li class="btn-hamburgerMenuColumn<?php if(is_category( 'culture' )): ?> btn-hamburgerMenuColumn-current<?php endif; ?>">
 						<a class="btn-hamburgerMenuColumnInr" href="/category/culture/">カルチャー</a><!-- /.btn-hamburgerMenuColumnInr -->
 					</li><!-- /.btn-hamburgerMenuColumn -->
 					
-					<li class="btn-hamburgerMenuColumn">
+					<li class="btn-hamburgerMenuColumn<?php if(is_category( 'news' )): ?> btn-hamburgerMenuColumn-current<?php endif; ?>">
 						<a class="btn-hamburgerMenuColumnInr" href="/category/news/">ニュース</a><!-- /.btn-hamburgerMenuColumnInr -->
 					</li><!-- /.btn-hamburgerMenuColumn -->
 					
-					<li class="btn-hamburgerMenuColumn">
+					<li class="btn-hamburgerMenuColumn<?php if(is_category( 'product' )): ?> btn-hamburgerMenuColumn-current<?php endif; ?>">
 						<a class="btn-hamburgerMenuColumnInr" href="/category/product/">プロダクト</a><!-- /.btn-hamburgerMenuColumnInr -->
 					</li><!-- /.btn-hamburgerMenuColumn -->
 				</ul><!-- /.list-hamburgerMenuColumn -->
