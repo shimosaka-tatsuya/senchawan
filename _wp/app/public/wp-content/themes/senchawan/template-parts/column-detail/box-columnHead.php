9a2cbb8ec1b357111342d422fbda56443ed75bf1
下坂　辰弥 <>
2019-03-21 17:51:23 +0900

    no message

--- _wp/app/public/wp-content/themes/senchawan/template-parts/column-detail/box-columnHead.php
+++ _wp/app/public/wp-content/themes/senchawan/template-parts/column-detail/box-columnHead.php
@@ -1,6 +1,6 @@
 <div class="box-columnHead">
 	<h1 class="ttl-columnHead">
-		<span class="txt-columnHead txt-columnHead-english"><?php the_field('ttlColumnEnglish'); ?></span>
+		<span class="txt-columnHead txt-columnHead-english txt-fontAlphabet"><?php the_field('ttlColumnEnglish'); ?></span>
 		<span class="txt-columnHead txt-columnHead-japanese"><?php the_title(); ?></span>
 	</h1><!-- /.ttl-columnHead -->
 	
